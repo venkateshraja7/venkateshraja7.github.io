b4def6b2e2d8cfb2eddb7da384673e2543fa645d
venkateshraja7 <125277273+venkateshraja7@users.noreply.github.com>
2023-03-28 23:05:22 +0530

    update

---
+++ week4/script.js
@@ -0,0 +1,15 @@
+var names=new Array();
+names[0]="yaakov";
+names[1]="jhon";
+names[2]="len";
+names[3]="miya";
+names[4]="jason";
+names[5]="paul";
+names[6]="larry";
+names[7]="marry";
+names[8]="shree";
+names[9]="jim";
+
+for (var i = 0; i < names.length; i++){
+   console.log(names[i]);
+}
